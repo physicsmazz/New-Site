a7af25ce8546867902489aa61e54032746660602
Mike Mazzantini <physicsmazz@gmail.com>
2012-01-12 10:56:39 -0500

    force chrome frame is IE7 and below.

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ require_once('includes/scripts.inc.php');
 </script>
 
 
-    <!--[if lt IE 7 ]>
+    <!--[if lt IE 8 ]>
       <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
       <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
     <![endif]-->
