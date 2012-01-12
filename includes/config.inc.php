d228458d29b6482707ce3722e476fad4658bdb04
Mike Mazzantini <physicsmazz@gmail.com>
2012-01-12 11:10:00 -0500

    cleaned up the base file a little.

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -8,15 +8,5 @@ function __autoload($class_name)
     require_once 'classes/' . $class_name . '.class.php';
 }
 
-// it's local, use the local db connection
-//if ($_SERVER['HTTP_HOST'] == 'localhost' || strstr($_SERVER['HTTP_HOST'], '192.168.2')) require_once('includes/connLocal.inc.php');
-//else require_once 'includes/conn.inc.php';
-//php logging
-$php = FirePHP::getInstance(true);
-//turn on php logging if local
-if ($_SERVER['HTTP_HOST'] == 'localhost' || strstr($_SERVER['HTTP_HOST'], '192.168.2')) {
-    $php->setEnabled(true);
-} else {
-    //set false for production.
-    $php->setEnabled(false);
-}
+//db connection
+//require_once 'includes/conn.inc.php';
--- includes/conn.inc.php
+++ includes/conn.inc.php
@@ -1,6 +1,16 @@
 <?php
-define('DB_SERVER', 'xx.xx.xx.xx');
-define('DB_USER', 'mazztodo');
-define('DB_PASSWORD', 'Mazz6299ToDo');
-define('DB_NAME', 'mazztodo');
+if ($_SERVER['HTTP_HOST'] == 'localhost' || strstr($_SERVER['HTTP_HOST'],'192.168.2') || $_SERVER['HTTP_HOST'] == '127.0.0.1'){
+    define('DB_SERVER', 'localhost');
+    define('DB_USER', 'physicsmazz');
+    define('DB_PASSWORD', 'mazz6288');
+    define('DB_NAME', 'todolist');
+}else{
+    define('DB_SERVER', 'xx.xx.xx.xx');
+    define('DB_USER', 'mazztodo');
+    define('DB_PASSWORD', 'Mazz6299ToDo');
+    define('DB_NAME', 'mazztodo');
+}
 $conn = new mySQLi (DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME) or die(mysqli_error());
+
+
+
