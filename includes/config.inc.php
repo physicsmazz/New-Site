e69ca9f5753b6db42744fdf69b1743a47e006037
Mike Mazzantini <physicsmazz@gmail.com>
2012-01-29 08:39:43 -0500

    Added debug class.

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -3,10 +3,12 @@ date_default_timezone_set('America/New_York');
 ob_start('ob_gzhandler');
 session_start();
 
+//require_once 'includes/private/conn.inc.php';
 function __autoload($class_name)
 {
     require_once 'classes/' . $class_name . '.class.php';
 }
 
-//db connection
-//require_once 'includes/conn.inc.php';
+////Logging
+//$debug = new PHPDebug();
+//$debug->debug('catArr', $catArr);
